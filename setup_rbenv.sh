8f6142f50cc613706947f06d25809c82f363e0f3
Hiroaki Nakamura <hnakamur@gmail.com>
2014-06-02 13:48:54 +0900

    Upgrade ruby to 2.1.1

--- setup_rbenv.sh
+++ setup_rbenv.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 anyenv install rbenv
 eval "$(anyenv init -)"
-rbenv install 2.1.1
-rbenv global 2.1.1
+rbenv install 2.1.2
+rbenv global 2.1.2
 eval "$(anyenv init -)"
 gem install bundler
 rbenv rehash
