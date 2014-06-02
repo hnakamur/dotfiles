c48068814694d1adc29bf45e2eba1649f4c96a2a
Hiroaki Nakamura <hnakamur@gmail.com>
2014-06-02 14:05:50 +0900

    Upgrade node.js to v0.10.28

--- setup_ndenv.sh
+++ setup_ndenv.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 anyenv install ndenv
 eval "$(anyenv init -)"
-ndenv install v0.10.26
-ndenv global v0.10.26
+ndenv install v0.10.28
+ndenv global v0.10.28
 eval "$(anyenv init -)"
 npm install -g grunt-cli
 ndenv rehash
